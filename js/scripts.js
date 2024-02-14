4d6409142c66aaca25d3ec5de6c3fc99e6572b25
Your Name <you@example.com>
2024-02-14 14:06:45 +0000

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+window.onscroll = function () {
+    myFunction()
+};
+const header = document.getElementById("topHeader");
+const sticky = header.offsetTop;
+
+function toggleMenu() {
+    const menu = document.getElementById("menu1");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
+
+function myFunction() {
+    if (window.scrollY > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
